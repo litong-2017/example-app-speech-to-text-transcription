2cbacb49ad399338834cd090013d801439052beb
LiTong <927545874@qq.com>
2022-10-08 23:37:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if f is not None:
         bytes_data = f.getvalue()
 
         # Load your API key from an environment variable or secret management service
-        api_token = st.secrets["tLjqWmYjmXqhsvEOANmK57wqIIe1EvAnUb4GdrCYHTqDkMeUokgdpg=="]
+        api_token = st.secrets["api_token"]
 
         # endregion API key
         headers = {"Authorization": f"Bearer {api_token}"}
